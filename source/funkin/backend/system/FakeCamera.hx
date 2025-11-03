f03b2a089b8242c8d4a9bd4f84d39e63d19ba1a4
Homura <egzozu.be1.bas@gmail.com>
2025-11-03 18:43:57 +0300

    wow

--- source/funkin/backend/system/FakeCamera.hx
+++ source/funkin/backend/system/FakeCamera.hx
@@ -5,7 +5,7 @@ import flixel.graphics.frames.FlxFrame;
 import flixel.graphics.tile.FlxDrawTrianglesItem;
 import flixel.math.FlxMatrix;
 import flixel.math.FlxPoint;
-import mobile.flixel.system.FlxShader;
+import flixel.system.FlxAssets.FlxShader;
 import openfl.display.BitmapData;
 import openfl.display.BlendMode;
 import openfl.geom.ColorTransform;
