55c153dbec2d24b6a378895981ed0be1ecabaac5
HEIHUAa <wudideheihua@outlook.com>
2025-12-28 20:36:38 +0800

    Remove mod files

--- mods/Starry Night Revelry/shaders/video_map.frag
+++
@@ -1,9 +0,0 @@
-#pragma header
-
-// by HeiHua
-
-uniform sampler2D video;
-
-void main() {
-	gl_FragColor = texture2D(video, openfl_TextureCoordv);
-}
--- mods/Starry Night Revelry/songs/OptimizeNoteGenTime.hx
+++
@@ -1,6 +0,0 @@
-function postUpdate(elapsed)
-{
-  var limit = 248 + 112 / (1 / camHUD.zoom) + 720 / scrollSpeed / camHUD.zoom + Conductor.stepCrochet;
-	for(strumLine in strumLines)
-  strumLine.notes.limit = limit;
-}
